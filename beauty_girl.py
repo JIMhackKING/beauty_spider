9749edcfc72b800d826a95b1755077788948ce17
DHC <a601767890@sina.com>
2019-09-16 02:00:45 +0800

    Change the saving place to seafile

--- beauty_girl.py
+++ beauty_girl.py
@@ -5,9 +5,12 @@ import time
 from threading import Thread
 from collections import deque
 from bs4 import BeautifulSoup
+import seafile_api
+from io import BytesIO
 
 session = requests.Session()
 img_url_list = deque()
+
 
 def get_girl_list():
     response = session.get("http://www.win4000.com/zt/xinggan.html")
@@ -24,6 +27,7 @@ def get_girl_list():
         get_girl_detail(name, url)
     else:
         get_girl_detail(None, None)
+
 
 # 解析图片详细
 def get_girl_detail(name, url):
@@ -42,8 +46,9 @@ def get_girl_detail(name, url):
         img_url = img_url[0:img_url.find('_')] + '.jpg'
         img_url_list.append([name, img_url])
 
+
 # 保存大图
-def save_image():
+def save_image(upload_link):
     name_bak = ""
     count = 0
     while True:
@@ -56,10 +61,7 @@ def save_image():
         if img_url == None:
             break
 
-        print img_url
         img_response = requests.get(img_url)
-        if not os.path.exists("images"):
-            os.mkdir("images")
         # 根据名字来排列
         if name != name_bak:
             name_bak = name
@@ -67,12 +69,28 @@ def save_image():
         else:
             count += 1
 
-        with open("images/%s_%d.jpg" %(name, count), 'wb') as f:
-            f.write(img_response.content)
+        file_obj = BytesIO(img_response.content)
+        file_obj.name = "images/%s_%d.jpg" % (name, count)
+        try:
+            file_id = seafile_api.upload_file(upload_link, file_obj, "/images")
+            print(img_url, '-----', file_id)
+        except:
+            print("error:", img_url)
+
 
 if __name__ == '__main__':
+    seafile_api.host = "www.vmnas.com:8000"
+    token = seafile_api.get_token({'username': 'a@a.com', 'password': '1'})
+    token = token['token']
+    repo_id = "195e8872-a019-41c6-9c67-dd12bc936293"
+    dir_name = "images"
+    dir_detail = seafile_api.get_dir(token, repo_id, dir_name)
+    if not dir_detail.get('name'):
+        seafile_api.create_dir(token, repo_id, dir_name)
+    upload_link = seafile_api.get_upload_link(token, repo_id)
+
     get_list_thread = Thread(target=get_girl_list)
-    get_img_thread = Thread(target=save_image)
+    get_img_thread = Thread(target=save_image, args=(upload_link,))
 
     get_list_thread.start()
     get_img_thread.start()
---
+++ seafile_api.py
@@ -0,0 +1,75 @@
+﻿import requests
+import io
+
+
+def get_token(data):
+    url = 'http://{host}/api2/auth-token/'.format(host=host)
+    res = requests.post(url, data=data)
+    res.raise_for_status()
+    return res.json()
+
+
+def get_upload_link(token, repo_id):
+    url = 'http://{host}/api2/repos/{repo_id}/upload-link/'.format(host=host, repo_id=repo_id)
+    res = requests.get(
+        url, headers={'Authorization': 'Token {token}'.format(token=token)}
+    )
+    res.raise_for_status()
+    return res.json()
+
+
+def list_libs(token):
+    url = 'http://{host}/api2/repos/'.format(host=host)
+    res = requests.get(
+        url, headers={'Authorization': 'Token {token}'.format(token=token)}
+    )
+    res.raise_for_status()
+    return res.json()
+
+
+def upload_file(upload_link, fp, parent_dir='/'):
+    if isinstance(fp, io.IOBase):
+        data = {'filename': fp.name.split('/')[-1], 'parent_dir': parent_dir}
+        files = {'file': fp}
+    elif isinstance(fp, str):
+        data = {'filename': fp.split('/')[-1], 'parent_dir': parent_dir}
+        files = {'file': open(fp, 'rb')}
+    else:
+        raise TypeError("fp type is invalid.")
+    res = requests.post(upload_link, data=data, files=files)
+    res.raise_for_status()
+    return res.text
+
+
+def create_dir(token, repo_id, dir_name):
+    url = "http://{host}/api2/repos/{repo_id}/dir/".format(host=host, repo_id=repo_id)
+    data = dict(operation="mkdir")
+    params = {"p": dir_name if dir_name.startswith("/") else "/"+dir_name}
+    headers = {'Authorization': 'Token {token}'.format(token=token)}
+    res = requests.post(url, data=data, params=params, headers=headers)
+    res.raise_for_status()
+    return res.text
+
+
+def get_dir(token, repo_id, dir_name):
+    url = "http://{host}/api/v2.1/repos/{repo_id}/dir/detail/".format(host=host, repo_id=repo_id)
+    headers = {'Authorization': 'Token {token}'.format(token=token)}
+    params = {"path": dir_name}
+    res = requests.get(url, headers=headers, params=params)
+    return res.json()
+
+
+if __name__ == '__main__':
+    host = 'www.vmnas.com:8000'
+    token = get_token({'username': 'a@a.com', 'password': '1'})
+    token = token['token']
+    libs = list_libs(token)
+    for lib in libs:
+        if lib.get('name') == "私人资料库":
+            repo_id = lib.get('id')
+            break
+    else:
+        repo_id = ''
+    # upload_link = get_upload_link(repo_id, token)
+    # print(upload_file(upload_link, 'test.py'))
+    print(get_dir(token, repo_id, "test"))
